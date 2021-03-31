f02bae57d32711b346fc18ed5d9013db47495d3f
inknsharps <nchen25@gmail.com>
2021-03-31 02:20:55 -0400

    add make profile objects and classes functionality

--- index.js
+++ index.js
@@ -3,53 +3,82 @@ const classes = require("./lib/classes.js");
 const ask = require("./lib/questions.js");
 const inquirer = require("inquirer");
 
-// Employee array that will get values pushed into it from the prompts
-let employeeArray = [];
+// Employee objects array
+let employeeProfiles = [];
+// Employee data array that will get values pushed into it from the prompts
+let employeeData = [];
+
+// Function to make a new object class for each role
+function makeProfile(role, name, id, email, extra){
+    if (role === "Manager"){
+        let newManager = new classes.Manager(name, id, email, extra);
+        return newManager;
+    }
+    if (role === "Engineer"){
+        let newEngineer = new classes.Engineer(name, id, email, extra);
+        return newEngineer;
+    }
+    if (role === "Intern"){
+        let newIntern = new classes.Intern(name, id, email, extra);
+        return newIntern;
+    }
+}
 
 // Terminal prompt logic flow
-function askInit(){
-    inquirer.prompt(ask.questions)
+async function askInit(){
+    // Set the returned inquirer prompt object to const newEmployee
+    const newEmployee = await inquirer.prompt(ask.questions);
+
+    // If manager was selected in the prompts, add the employee_office to the newEmployee object with a new prompt
+    if (newEmployee.employee_role === "Manager"){
+        await inquirer.prompt(ask.managerQuestion)
+            .then((response) => {
+                newEmployee.employee_extra = response.employee_office;
+            })
+            .catch((error) => {
+                if (error) throw error;
+            })
+    }
+    // If engineer was selected int the prompts, add the employee_github to the newEmployee object with a new prompt
+    if (newEmployee.employee_role === "Engineer"){
+        await inquirer.prompt(ask.engineerQuestion)
+            .then((response) => {
+                newEmployee.employee_extra = response.employee_github;
+            })
+            .catch((error) => {
+                if (error) throw error;
+            })
+    }
+    // If intern was selected in the prompts, add the employee_school to the newEmployee object with a new prompt
+    if (newEmployee.employee_role === "Intern"){
+        await inquirer.prompt(ask.internQuestion)
+            .then((response) => {
+                newEmployee.employee_extra = response.employee_school;
+            })
+            .catch((error) => {
+                if (error) throw error;
+            })
+    }
+    // Push the new employee to the employeeData
+    employeeData.push(newEmployee);
+    console.log("Current employee array is:", employeeData);
+    // Ask the user if they would like to add another employee, and loops this function if they say yes
+    await inquirer.prompt(ask.addAnotherEmployee)
         .then((response) => {
-            const newEmployee = response;
-            // If manager was selected, add the employee_office to the newEmployee object with a new prompt
-            if (newEmployee.employee_role === "Manager"){
-                inquirer.prompt(ask.managerQuestion)
-                    .then((response) => {
-                        newEmployee.employee_office = response.employee_office;
-                        console.log(newEmployee);
-                        return newEmployee;
-                    })
+            if (response.addAnother === true){
+                askInit();
+            } else {
+                console.log("Generating employee profiles...");
+                // For loop that makes a new employee class object based on the data in employeeData, and pushes it to the employeeProfiles array
+                for (let i = 0; i < employeeData.length; i++){
+                    const newProfile = makeProfile(employeeData[i].employee_role, employeeData[i].employee_name, employeeData[i].employee_id, employeeData[i].employee_email, employeeData[i].employee_extra)
+                    employeeProfiles.push(newProfile);
+                }
+                console.log(employeeProfiles)
             }
-            // If engineer was selected, add the employee_github to the newEmployee object with a new prompt
-            if (newEmployee.employee_role === "Engineer"){
-                inquirer.prompt(ask.engineerQuestion)
-                    .then((response) => {
-                        newEmployee.employee_github = response.employee_github;
-                        console.log(newEmployee);
-                        return newEmployee;
-                    })
-            }
-            if (newEmployee.employee_role === "Intern"){
-                inquirer.prompt(ask.internQuestion)
-                    .then((response) => {
-                        newEmployee.employee_school = response.employee_school;
-                        console.log(newEmployee);
-                        return newEmployee;
-                    })
-            }
-            return newEmployee;
         })
-        .then((response) => {
-            employeeArray.push(response);
-            console.log("Current employee array is:", employeeArray);
-            inquirer.prompt(ask.addAnotherEmployee)
-                .then((response) => {
-                    if (response.addAnother === true){
-                        askInit();
-                    } else {
-                        console.log("Thanks for ending my suffering")
-                    }
-                })
+        .catch((error) => {
+            if (error) throw error;
         })
 }
 
