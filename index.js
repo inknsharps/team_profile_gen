4e4eeead98713e3879a69675edb008a9f1673125
inknsharps <nchen25@gmail.com>
2021-04-03 02:41:46 -0400

    make minor refactors for functional code

--- index.js
+++ index.js
@@ -16,14 +16,11 @@ let employeeCards = [];
 function makeProfile(role, name, id, email, extra){
     switch (role){
         case "Manager":
-            let newManager = new classes.Manager(name, id, email, extra);
-            return newManager;
+            return new classes.Manager(name, id, email, extra);
         case "Engineer":
-            let newEngineer = new classes.Engineer(name, id, email, extra);
-            return newEngineer;
+            return new classes.Engineer(name, id, email, extra);
         case "Intern":
-            let newIntern = new classes.Intern(name, id, email, extra);
-            return newIntern;
+            return new classes.Intern(name, id, email, extra);
     }
 }
 
@@ -65,7 +62,6 @@ async function askInit(){
 
     // Push the new employee to the employeeData
     employeeData.push(newEmployee);
-    console.log("Current employee array is:", employeeData);
 
     // Ask the user if they would like to add another employee, and loops this function if they say yes, otherwise, proceed with making the html
     await inquirer.prompt(ask.addAnotherEmployee)
@@ -74,37 +70,34 @@ async function askInit(){
                 askInit();
             } else {
                 console.log("Generating employee profiles...");
+                
                 // For of statement that makes a new employee class object based on the data in employeeData, and pushes it to the employeeObjects array
                 for (const index of employeeData){
                     const newProfile = makeProfile(index.employee_role, index.employee_name, index.employee_id, index.employee_email, index.employee_extra);
                     employeeObjects.push(newProfile);
                 }
-                console.log("Current employee objects array is:", employeeObjects);
+
                 // For of statement that iterates through the array of employee objects, and runs the renderTeamCards function based on the role, and pushes the returned card into the employeeCards array
                 for (const index of employeeObjects){
-                    let card;
                     switch (index.role){
                         case "Manager":
-                            card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.officeNumber);
-                            employeeCards.push(card);
+                            employeeCards.push(templates.renderTeamCards(index.name, index.role, index.id, index.email, index.officeNumber));
                             break;
                         case "Engineer":
-                            card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.github);
-                            employeeCards.push(card);
+                            employeeCards.push(templates.renderTeamCards(index.name, index.role, index.id, index.email, index.github));
                             break;
                         case "Intern":
-                            card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.school);
-                            employeeCards.push(card);
+                            employeeCards.push(templates.renderTeamCards(index.name, index.role, index.id, index.email, index.school));
                             break;
                     }
                 }
-                console.log("Employee cards HTML data is:", employeeCards);
                 console.log("Rendering HTML...")
 
                 // Join together the employeeCards array data, and make a HTML file from that data
                 let employeeCardsHTML = employeeCards.join("");
                 let finalHTMLData = templates.renderBoilerplateHTML(employeeCardsHTML);
                 fs.writeFileSync("./dist/teams.html", finalHTMLData);
+                console.log("File written to ./dist/teams.html !");
             }
         })
         .catch((error) => {
