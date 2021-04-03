d439f62c915112f708a959b798fe7c09caadabee
inknsharps <nchen25@gmail.com>
2021-04-03 02:28:04 -0400

    refactor if else statements to switch for readability

--- index.js
+++ index.js
@@ -14,17 +14,16 @@ let employeeCards = [];
 
 // Function to make a new object class for each role
 function makeProfile(role, name, id, email, extra){
-    if (role === "Manager"){
-        let newManager = new classes.Manager(name, id, email, extra);
-        return newManager;
-    }
-    if (role === "Engineer"){
-        let newEngineer = new classes.Engineer(name, id, email, extra);
-        return newEngineer;
-    }
-    if (role === "Intern"){
-        let newIntern = new classes.Intern(name, id, email, extra);
-        return newIntern;
+    switch (role){
+        case "Manager":
+            let newManager = new classes.Manager(name, id, email, extra);
+            return newManager;
+        case "Engineer":
+            let newEngineer = new classes.Engineer(name, id, email, extra);
+            return newEngineer;
+        case "Intern":
+            let newIntern = new classes.Intern(name, id, email, extra);
+            return newIntern;
     }
 }
 
@@ -33,70 +32,79 @@ async function askInit(){
     // Set the returned inquirer prompt object to const newEmployee
     const newEmployee = await inquirer.prompt(ask.questions);
 
-    // If manager was selected in the prompts, add the employee_office to the newEmployee object with a new prompt
-    if (newEmployee.employee_role === "Manager"){
-        await inquirer.prompt(ask.managerQuestion)
+    // Switch statement that adds an additional property to the object, based on the role
+    switch (newEmployee.employee_role){
+        case "Manager":
+            await inquirer.prompt(ask.managerQuestion)
             .then((response) => {
                 newEmployee.employee_extra = response.employee_office;
             })
             .catch((error) => {
                 if (error) throw error;
             })
-    }
-    // If engineer was selected int the prompts, add the employee_github to the newEmployee object with a new prompt
-    if (newEmployee.employee_role === "Engineer"){
-        await inquirer.prompt(ask.engineerQuestion)
+            break;
+        case "Engineer":
+            await inquirer.prompt(ask.engineerQuestion)
             .then((response) => {
                 newEmployee.employee_extra = response.employee_github;
             })
             .catch((error) => {
                 if (error) throw error;
             })
-    }
-    // If intern was selected in the prompts, add the employee_school to the newEmployee object with a new prompt
-    if (newEmployee.employee_role === "Intern"){
-        await inquirer.prompt(ask.internQuestion)
+            break;
+        case "Intern":
+            await inquirer.prompt(ask.internQuestion)
             .then((response) => {
                 newEmployee.employee_extra = response.employee_school;
             })
             .catch((error) => {
                 if (error) throw error;
             })
+            break;
     }
 
     // Push the new employee to the employeeData
     employeeData.push(newEmployee);
     console.log("Current employee array is:", employeeData);
 
-    // Ask the user if they would like to add another employee, and loops this function if they say yes
+    // Ask the user if they would like to add another employee, and loops this function if they say yes, otherwise, proceed with making the html
     await inquirer.prompt(ask.addAnotherEmployee)
         .then((response) => {
             if (response.addAnother === true){
                 askInit();
             } else {
                 console.log("Generating employee profiles...");
-                // For loop that makes a new employee class object based on the data in employeeData, and pushes it to the employeeObjects array
-                for (let i = 0; i < employeeData.length; i++){
-                    const newProfile = makeProfile(employeeData[i].employee_role, employeeData[i].employee_name, employeeData[i].employee_id, employeeData[i].employee_email, employeeData[i].employee_extra)
+                // For of statement that makes a new employee class object based on the data in employeeData, and pushes it to the employeeObjects array
+                for (const index of employeeData){
+                    const newProfile = makeProfile(index.employee_role, index.employee_name, index.employee_id, index.employee_email, index.employee_extra);
                     employeeObjects.push(newProfile);
                 }
-                console.log(employeeObjects);
-                // For of statement that iterates through the array of employee objects, and runs the renderTeamCards function, and pushes the returned card into the employeeCards array
+                console.log("Current employee objects array is:", employeeObjects);
+                // For of statement that iterates through the array of employee objects, and runs the renderTeamCards function based on the role, and pushes the returned card into the employeeCards array
                 for (const index of employeeObjects){
-                    if (index.role === "Manager"){
-                        let card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.officeNumber);
-                        employeeCards.push(card);
-                    }
-                    if (index.role === "Engineer"){
-                        let card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.github);
-                        employeeCards.push(card);
-                    }
-                    if (index.role === "Intern"){
-                        let card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.school);
-                        employeeCards.push(card);
+                    let card;
+                    switch (index.role){
+                        case "Manager":
+                            card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.officeNumber);
+                            employeeCards.push(card);
+                            break;
+                        case "Engineer":
+                            card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.github);
+                            employeeCards.push(card);
+                            break;
+                        case "Intern":
+                            card = templates.renderTeamCards(index.name, index.role, index.id, index.email, index.school);
+                            employeeCards.push(card);
+                            break;
                     }
                 }
-                console.log(employeeCards);
+                console.log("Employee cards HTML data is:", employeeCards);
+                console.log("Rendering HTML...")
+
+                // Join together the employeeCards array data, and make a HTML file from that data
+                let employeeCardsHTML = employeeCards.join("");
+                let finalHTMLData = templates.renderBoilerplateHTML(employeeCardsHTML);
+                fs.writeFileSync("./dist/teams.html", finalHTMLData);
             }
         })
         .catch((error) => {
--- src/renderhtml.js
+++ src/renderhtml.js
@@ -12,7 +12,12 @@ const htmlTemplate = `
     <title>My Team</title>
 </head>
 <body>
-    ${content}
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <h1>My Team</h1>
+    </nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <main>
+        ${content}
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 </html>
